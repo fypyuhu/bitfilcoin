76f780560ea459220abc075e583134b5dc6ea83a
root <root@ip-172-31-45-163.eu-central-1.compute.internal>
2018-06-04 11:05:12 +0000

    checklist

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -36,12 +36,22 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         (  0, uint256("0x937c2c816305b15d5f0a55477e3888cf4c7ae30a13aaedcba089804abff7a886"))
-       
+        (  10, uint256("0x6ceb228dcbaceecf03bd68c753644eebef26d3cce61430db47dc141f39ba5507"))
+        (  20, uint256("0xd76463183e85c92c6791ae3ca87f7174f7b1ce4cdaffc9a3fb924aa57c788207"))
+        (  30, uint256("0xabc7151fd7e8d490fe03df26aebf152ba7b4a9be1cf982e8aa4c1fd6d54b6c31"))
+        (  40, uint256("0x16049e975baf72be5961ac587732b1706c2e23705701e1a5d5399a1956c2308e"))
+        (  50, uint256("0xd81c2290bb1081ae57bdd7a0874d4ff8c91c54c97ffbe539850578c0ce68a0c6"))
+        (  60, uint256("0x90c80d15a6ba9c8d7ee9ed2ed168b3a37b8184974ffd50b903394f86ac8a82b3"))
+        (  70, uint256("0xa3b1ffbe9245206c7cd3d0288540c6f352adad892db3cc16f2dcab4bf8ac8ac7"))
+        (  80, uint256("0x28ac757a9ffff3885729355df4518d07e98799cd91699e1dc12837a7baf4f9a9"))
+        (  90, uint256("0x5c768f448f4428bca7f67aa9faffb094bb674a39ac163b21d8b67b0477f03180"))
+        (  100, uint256("0x4f0551b8a2aadaa414c1923b1f40eb85f2159b27a57653fc0c8f0d8073b8cfee"))
+        (  110, uint256("0x0e3093f26fb32dbb08d44a5c9a40e7b31f4320e22572f4eef6856349e4f7d90b"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
         1526335548, // * UNIX timestamp of last checkpoint block
-        0,    // * total number of transactions between genesis and last checkpoint
+        110,    // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         1.0     // * estimated number of transactions per day after checkpoint
     };
